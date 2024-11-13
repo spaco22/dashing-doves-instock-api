36a55169486c1987cea5a941687a6b8196d55858
spaco22 <58753896+spaco22@users.noreply.github.com>
2024-11-13 09:56:15 -0800

    create GET warehouse by id route

--- routes/warehouses.js
+++ routes/warehouses.js
@@ -6,4 +6,8 @@ warehouseRoute.get("/", (req, res) => {
   res.send("This is the warehouse route!");
 });
 
+warehouseRoute.get("/:id", (req, res) => {
+  res.send("This is the GET warehouse by id route!");
+});
+
 export default warehouseRoute;
