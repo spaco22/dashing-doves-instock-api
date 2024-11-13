359fcf4539bf294d78d93910688afc5760903650
Tanya Vezyr <tetiana.vezyr@gmail.com>
2024-11-13 13:12:24 -0700

    add controllers, implement GET all warehouses

--- routes/warehouses.js
+++ routes/warehouses.js
@@ -1,38 +1,10 @@
 import express from "express";
-import initKnex from "knex";
-import configuration from "../knexfile.js";
-const knex = initKnex(configuration);
+import * as warehouseController from "../controllers/warehouse-controller.js";
 
-const warehouseRoute = express.Router();
+const router = express.Router();
 
-warehouseRoute.get("/", (req, res) => {
-  res.send("This is the warehouse route!");
-});
+router.route("/").get(warehouseController.index);
 
+router.route("/:id").get(warehouseController.findOne);
 
-// NOTE: USED .ROUTE TO CHAIN OTHER REQUESTS :)
-warehouseRoute.route("/:id").get(async (req, res) => {
-  try {
-    const warehouseData = await knex("warehouses")
-      .where({ id: req.params.id })
-      .first();
-
-    if (warehouseData.length === 0) {
-      console.log(`Error finding warehouse id ${req.params.id}`);
-      return res.status(404).json({
-        message: `Warehouse with ID ${req.params.id} not found`,
-        status: 404,
-      });
-    }
-
-    res.status(200).json(warehouseData);
-  } catch (error) {
-    console.error("Error retrieving warehouse data", error);
-    res.status(500).json({
-      message: "Error retrieving warehouse data",
-      status: 500,
-    });
-  }
-});
-
-export default warehouseRoute;
+export default router;
