65ff82e5a205871eba0ff455adb7e0f24b4bc65a
spaco22 <58753896+spaco22@users.noreply.github.com>
2024-11-14 10:09:59 -0800

    create add warehouse api call

--- controllers/warehouse-controller.js
+++ controllers/warehouse-controller.js
@@ -4,8 +4,8 @@ const knex = initKnex(configuration);
 
 const index = async (_req, res) => {
   try {
-    const data = await knex("warehouses");
-    res.status(200).json(data);
+    const warehousesData = await knex("warehouses");
+    res.status(200).json(warehousesData);
   } catch (error) {
     console.error(`Error retrieving Warehouses: ${error}`);
     res.status(400).json({ message: "Error retrieving Warehouses" });
@@ -14,8 +14,9 @@ const index = async (_req, res) => {
 
 const findOne = async (req, res) => {
   try {
-    const warehousesFound = await knex("warehouses")
-      .where({ id: req.params.id });
+    const warehousesFound = await knex("warehouses").where({
+      id: req.params.id,
+    });
 
     if (warehousesFound.length === 0) {
       return res.status(404).json({
@@ -34,4 +35,31 @@ const findOne = async (req, res) => {
   }
 };
 
-export { index, findOne };
+const add = async (req, res) => {
+
+  if (!req.body.warehouse_name || !req.body.address || !req.body.city || !req.body.country  || !req.body.contact_name || !req.body.contact_position || !req.body.contact_phone || !req.body.contact_email ) {
+    return res.status(400).json({
+      message: "Please provide warehouse name, address, city, country, as well as contact name, position, phone number and email for create warehouse request",
+      status: 400
+    });
+  }
+
+  try {
+    const warehousesData = await knex("warehouses").insert(req.body);
+    const newWarehouseId = warehousesData[0];
+    const newWarehouse = await knex("warehouses").where({ id: newWarehouseId }).first();
+
+    res.status(201).json({
+      message: "Warehouse succesfully created",
+      newWarehouse,
+    });
+  } catch (error) {
+    console.error("Error creating new warehouse", error);
+    res.status(500).json({
+      message: "Error creating new warehouse",
+      status: 500,
+    });
+  }
+};
+
+export { index, findOne, add };
